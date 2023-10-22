832288775d1d74adbe72dd278fc3612bd0adc082
Biethe <147930989+Biethe@users.noreply.github.com>
2023-10-22 11:01:51 +0200

    Add a few comments

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,6 +1,7 @@
 #include "Utils.h"
 #include "Date.h"
 
+/*Independent function to assess the chronologic order of two given dates*/
 bool before(Date d1, Date d2) {
     if (d1.get_year() < d2.get_year()) {
         return true;
@@ -13,12 +14,13 @@ bool before(Date d1, Date d2) {
     }
 }
 
-
+/*Independent function to compute the number of days that have passed between two given dates (assuming the second passed date took place earlier in time )*/
 int difference(Date d1, Date d2){
 	int duration = (d1.get_day() - d2.get_day()) + (d1.get_month() - d2.get_month())*30 + (d1.get_year() - d2.get_year())*365;
 	return duration;
 }
 
+/*Independent function to compute the number of days that have passed between two given dates*/
 int duration(Date d1, Date d2){
 	if (before(d1, d2) == false){
 		return difference(d1, d2);
@@ -28,10 +30,12 @@ int duration(Date d1, Date d2){
 	}
 }
 
+/*Overload of operator '<' which basically return the same value as function 'before'.*/
 bool operator<(Date &d1,Date d2){
 	return before(d1, d2);
 }
 
+/*Overload of operator '-' which basically return the same value as function 'difference'*/
 int operator-(Date &d1,Date d2){
 	return difference(d1, d2);
 }
